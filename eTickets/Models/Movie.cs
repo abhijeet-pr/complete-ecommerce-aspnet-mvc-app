2d6c869766ac5d5cfd60093d1e13eedfd5432b4e
abhijeet <abhijeet.n@test.com>
2025-11-22 21:45:53 +0530

    Initial Model changes

---
+++ eTickets/Models/Movie.cs
@@ -0,0 +1,19 @@
+﻿using eTickets.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int MovieId { get; set; }
+
+        public string MovieName { get; set; }
+        public string MovieDescription { get; set; }
+        public double Price { get; set; }
+        public string ImageURL { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public MovieCategory MovieCategory { get; set; }
+    }
+}
